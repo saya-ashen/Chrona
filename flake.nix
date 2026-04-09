0383df7b961293172a9103c570cb1bd84f67423d
saya <c3313433633@gmail.com>
2026-04-09 16:49:44 +0800

    chore: ignore local development state
    Ignore direnv and the local Prisma SQLite database while adding the TypeScript language server to the Nix dev shell so local tooling changes stop polluting feature commits.

--- flake.nix
+++ flake.nix
@@ -6,57 +6,61 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        playwrightLibs = with pkgs; [
-          glib
-          nspr
-          nss
-          atk
-          at-spi2-atk
-          at-spi2-core
-          dbus
-          expat
-          libdrm
-          libgbm
-          mesa
-          libxkbcommon
-          alsa-lib
-          udev
-          cups
-          gtk3
-          pango
-          cairo
-          xorg.libX11
-          xorg.libXcomposite
-          xorg.libXdamage
-          xorg.libXext
-          xorg.libXfixes
-          xorg.libXrandr
-          xorg.libxcb
-        ];
-      in {
-        devShells.default = pkgs.mkShell {
-          packages =
-            (with pkgs; [
-              bun
-              nodejs_22
-              prisma-engines
-              uv
-              python313
-              sqlite
-              pkg-config
-              openssl
-            ])
-            ++ playwrightLibs;
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      playwrightLibs = with pkgs; [
+        glib
+        nspr
+        nss
+        atk
+        at-spi2-atk
+        at-spi2-core
+        dbus
+        expat
+        libdrm
+        libgbm
+        mesa
+        libxkbcommon
+        alsa-lib
+        udev
+        cups
+        gtk3
+        pango
+        cairo
+        xorg.libX11
+        xorg.libXcomposite
+        xorg.libXdamage
+        xorg.libXext
+        xorg.libXfixes
+        xorg.libXrandr
+        xorg.libxcb
+      ];
+    in {
+      devShells.default = pkgs.mkShell {
+        packages =
+          (with pkgs; [
+            bun
+            nodejs_22
+            prisma-engines
+            uv
+            python313
+            sqlite
+            pkg-config
+            openssl
+            typescript-language-server
+          ])
+          ++ playwrightLibs;
 
-          shellHook = ''
-            export PLAYWRIGHT_BROWSERS_PATH="''${PLAYWRIGHT_BROWSERS_PATH:-$HOME/.cache/ms-playwright}"
-            export PRISMA_SCHEMA_ENGINE_BINARY="${pkgs.prisma-engines}/bin/schema-engine"
-            export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath playwrightLibs}:''${LD_LIBRARY_PATH:-}"
-          '';
-        };
-      });
+        shellHook = ''
+          export PLAYWRIGHT_BROWSERS_PATH="''${PLAYWRIGHT_BROWSERS_PATH:-$HOME/.cache/ms-playwright}"
+          export PRISMA_SCHEMA_ENGINE_BINARY="${pkgs.prisma-engines}/bin/schema-engine"
+          export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath playwrightLibs}:''${LD_LIBRARY_PATH:-}"
+        '';
+      };
+    });
 }
