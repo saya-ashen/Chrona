6c5018b15d08d04893d31d210dcc1498bb61e29e
saya <c3313433633@gmail.com>
2026-07-11 23:46:05 +0800

    architecture: publish schedule engine contracts

--- features/schedule/index.ts
+++ features/schedule/index.ts
@@ -2,15 +2,15 @@ export * from "./contract";
 export * from "./routes/page.routes";
 export * from "./routes/task.routes";
 export {
+  applySchedule,
+  clearSchedule,
+  decideScheduleProposal,
+  deriveAutoStartEligibility,
+  getSchedulePage,
+  proposeSchedule,
   TaskScheduling,
   taskScheduling,
-} from "../../packages/engine/src/modules/scheduling/task-scheduling";
-export { applySchedule } from "../../packages/engine/src/modules/scheduling/apply-schedule";
-export { clearSchedule } from "../../packages/engine/src/modules/scheduling/clear-schedule";
-export { proposeSchedule } from "../../packages/engine/src/modules/scheduling/propose-schedule";
-export { decideScheduleProposal } from "../../packages/engine/src/modules/scheduling/decide-schedule-proposal";
-export { getSchedulePage } from "../../packages/engine/src/modules/pages/get-schedule-page";
-export { deriveAutoStartEligibility } from "../../packages/engine/src/modules/scheduling/derive-auto-start-eligibility";
+} from "@chrona/engine";
 export type { TaskConfigAiClient, TaskConfigExecutionRuntime, TaskConfigFormInput } from "./ui/forms/task-config-form";
 export type { SchedulePageData } from "./ui/schedule-page-types";
 export { TaskCreateDialog } from "./ui/dialogs/task-create-dialog";
